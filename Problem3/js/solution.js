f5e342ac8398e394c1a71c96da230fa978443974
IgnacioRubio <ignaciorubio3@gmail.com>
2019-07-09 22:11:16 +0200

    Last commit

--- Problem3/js/solution.js
+++ Problem3/js/solution.js
@@ -1,1 +1,12 @@
 // implement here your solution for the problem
+
+var number = 10;
+var temp = 0;
+var prime = 2;
+
+var diff = number / prime;
+
+
+
+// check interger numbers
+Number.isInteger(diff)
